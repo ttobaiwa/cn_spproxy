da8df2d198352fba2122f9e97beb7b88481edbae
ttobaiwa <tariro.tobaiwa@cassavasmartech.co.zw>
2022-02-02 19:41:14 +0000

    clean-up

--- routes/api/incident.js
+++ routes/api/incident.js
@@ -2,57 +2,8 @@ const express = require("express");
 const router = express.Router();
 const dbInst = require("../../services/db");
 
-/* LIST all incidents */
-router.get("/", async function (req, res, next) {
-  try {
-    const db = require("../../services/db").dbConnection();
-    db.all(
-      `SELECT date, description, source, severity FROM incident ORDER BY id`,
-      [],
-      (err, rows) => {
-        if (err) {
-          //throw err.message;
-          res.status(400).json({
-            error: true,
-            message: err.message,
-          });
-          db.close();
-          return;
-        }
-        if (rows.length > 0) {
-          console.log("some rows");
-          res.status(200).json({
-            error: false,
-            data: rows,
-          });
-          db.close();
-          return;
-        } else {
-          console.log("no rows");
-          res.status(400).json({
-            error: true,
-            message: err.message,
-          });
-          db.close();
-          return;
-        }
-      }
-    );
-  } catch (err) {
-    console.error(`Error while fetching task list`, err.message);
-    res.status(400).json({ error: true, message: err.message });
-    next(err);
-  }
-});
-
-/* LIST all tasks */
+/* LIST all incidents from a year */
 router.get("/stats/:year", async function (req, res, next) {
-  // res.status(200).json({
-  //   error: false,
-  //   incidents: getIncidents(req.params.year),
-  // });
-  // return;
-
   getIncidents(req.params.year, function (err, result1) {
     if (err) {
       res.status(400).json({
@@ -78,22 +29,6 @@ router.get("/stats/:year", async function (req, res, next) {
       });
     }
   });
-});
-
-/* POST NEW incident */
-router.post("/", async function (req, res, next) {
-  try {
-    const resp = await dbInst.createIncidentLog(req.body);
-    if (!resp.error) {
-      res.status(200).json(resp);
-    } else {
-      res.status(400).json(resp);
-    }
-  } catch (err) {
-    console.error(`Error while sending message `, err.message);
-    res.status(400).json({ error: true, message: err.message });
-    next(err);
-  }
 });
 
 function getIncidents(selectedYear, callback) {
